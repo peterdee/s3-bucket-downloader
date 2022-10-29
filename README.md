2d922adac8ffd8c5dd99601e87c3dd1991170378
Peter <peter.dyumin@gmail.com>
2022-10-30 02:28:55 +0300

    Improvements

---
+++ README.md
@@ -0,0 +1,26 @@
+## s3-bucket-downloader
+
+Download all of the contents of the Amazon S3 bucket
+
+### Deploy
+
+```shell script
+git clone https://github.com/peterdee/s3-bucket-downloader
+cd ./s3-bucket-downloader
+nvm use 18
+npm ci
+```
+
+### Environment variables
+
+The `.env` file is required, see [.env.example](.env.example) for details
+
+### Launch
+
+```shell script
+npm start
+```
+
+### License
+
+[MIT](./LICENSE.md)
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const {
   BUCKET,
   REGION,
   SECRET_KEY,
-} = process.env; 
+} = process.env;
 
 if (!(ACCESS_KEY && BUCKET && REGION && SECRET_KEY)) {
   throw new Error('Missing required Amazon keys!');
@@ -46,10 +46,17 @@ async function download(nextContinuationToken = '') {
     NextContinuationToken: token = '',
   } = await S3.listObjectsV2(params).promise();
   if (list.length === 0) {
+    // TODO: better way to stop downloading
+    log('Done!');
     return process.exit(0);
   }
 
-  await Promise.all(list.map(async ({ Key = '' }) => {
+  // const filtered = list.filter(
+  //   ({
+  //     Key = '',
+  //   }) => !Key.includes('main') && !Key.includes('preview') && !Key.includes('thumb'),
+  // );
+  await Promise.all(filtered.map(async ({ Key = '' }) => {
     const fileName = Key.split('/').join('_');
     const data = await S3.getObject({
       Bucket: BUCKET,
@@ -60,6 +67,7 @@ async function download(nextContinuationToken = '') {
         `${DESTINATION}/${fileName}`,
         data.Body,
       );
+
       counter += 1;
       log(`File ${counter} > ${fileName}`)
     }
@@ -76,6 +84,7 @@ async function launch() {
   } catch (error) {
     if (error.code && error.code === 'ENOENT') {
       await mkdir(DESTINATION);
+      log(`Created directory for downloaded files: ${DESTINATION}`);
 
       return download();
     }
